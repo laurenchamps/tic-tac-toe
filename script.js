fade9a21bd18fd0a90c6e622a3b2f928ea8c33b3
Lauren Champs <laurenkchamps@gmail.com>
2023-09-12 16:15:59 +0930

    Display which player's turn it is on screen

--- script.js
+++ script.js
@@ -9,46 +9,47 @@ const Player = (marker) => {
     return { marker };
 };
 
+
 const playGame = (() => {
-    // Create players
     const playerX = Player('X');
     const player0 = Player('0');
-
-    let turnTracker = 'X';
+    
+    let currentPlayer = "X";
     let round = 0;
-    let currentPlayer = "";
     let result;
+    
+    displayTurn(currentPlayer);
 
     const takeTurn = (e) => {
         let boardIndex = e.target.getAttribute("data-index");
-        setCurrentPlayerMarker(turnTracker);
         addMarkerToBoard(e, currentPlayer);
         updateBoard(e, currentPlayer, boardIndex);
-
         round++;
+        
         let winner = checkWinner(boardIndex, currentPlayer);
-
+        
         if (winner) {
             result = `${currentPlayer} wins`;
             displayResult();
             endGame();
         };
-
+        
         if (round === 9) {
             result = 'It\'s a draw';
             displayResult();
             endGame();
         };
-
+        
         switchTurn(currentPlayer);
+        displayTurn(currentPlayer);
     };
 
-    const setCurrentPlayerMarker = (turn) => {
-        currentPlayer = turn;
-    };
+    function displayTurn(currentPlayer) {
+        return document.querySelector('.display-turn').textContent = `Player ${currentPlayer}\'s turn`;
+    }
 
-    const switchTurn = (currentPlayer) => {
-        currentPlayer === 'X' ? turnTracker = '0' : turnTracker = 'X';
+    const switchTurn = (player) => {
+        player === 'X' ? currentPlayer = '0' : currentPlayer = 'X';
     };
 
     const addMarkerToBoard = (e, currentPlayer) => {
@@ -91,78 +92,4 @@ const playGame = (() => {
         }); 
 
 })();
-
-
-
-
-
-// // // Create a 3x3 grid of divs on the page with unique attribute eg data-row and data-column
-
-// // // Create an object for creating a player
-
-// // const marker = document.querySelectorAll('.marker');
-
-// // function assignMarker(e) {
-// //     console.log(e.target.textContent);
-
-
-// //     if (e.target.textContent === 'X') {
-// //         return 'X';
-// //     } else {
-// //         return '0';
-// //     }
-
-// // }
-
-// // marker.forEach((item) => {
-// //     item.addEventListener('click', assignMarker)});
-
-// // const playerMarker = assignMarker();
-// // console.log(playerMarker);
-
-// // const Player = (name, marker) => {
-   
-// //     const selectSquare = () => console.log(marker); // Replace this with what I need this to do
-
-
-// //     return { name, marker, selectSquare };
-// //   };
-
-// //   // Create player 1
-// //   // Assign marker to player 1
-// // // const player1 = Player('Player 1', assignMarker());
-
-// // // Create player 2
-// // // Assign marker to player 2
-// // const player2 = Player('Player 2', '0');
-
-
-// // // Populate data structure with 'null' to start
-// // // Player selects move (play()) ==> can be either play('0') or play('x') depending on which player is playing
-// // // Listen to all divs in gameboard and when one is clicked run play()
-// // // Add corresponding marker to correct location on grid in DOM (updateBoard() or displayMarker()?)
-// // // Player can only select square that has null value
-// // // Game alternates between players (? some counter that switches between x and 0 to indicate whose turn it is? or setTurn() to switch to other player?)
-
-// // // Create object for controlling play
-// // const game = {
-// //     playRound: () => {
-// //         player1.selectSquare();
-// //         player2.selectSquare();
-// //         player2.selectSquare();
-// //     },
-// //     checkWinner: () => {
-// //         console.log(Gameboard.board);
-// //     }
-// // };
-
-
-
-// // game.playRound();
-// // game.checkWinner();
-// // // Run checkWinner() to check if anyone has won
-
-// // // If winner, displayWinner() to display winning message on screen and end game
-// // // When data structure no longer contains any null values, if there is no winner declare a draw and game over.
-
 
